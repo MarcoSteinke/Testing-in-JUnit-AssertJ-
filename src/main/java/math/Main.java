c7073927acb4734dfb7d8d468b13d93f64a18ecf
maste150hhu <mastremolten2@gmail.com>
2020-06-27 19:55:13 +0200

    refactor / rearrange code

--- src/main/java/math/Main.java
+++ src/main/java/math/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         // sum of 1, 2, 3
-                double result = Sum.of(1, 2, 3);
+        double result = Sum.of(1, 2, 3);
         // 3rd root of 8
         double root = NthRoot.of(3, 8);
 
